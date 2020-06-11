e54b887844a9ee53bcf5feebe51b9208d3851c2e
Dibyendu Das <43660480+Dibyendu1000@users.noreply.github.com>
2020-06-11 17:41:52 +0530

    Fix: Fix problem of some valid EMail Id not being accepted

--- libs/firebase_insert.py
+++ libs/firebase_insert.py
@@ -1,10 +1,12 @@
 from firebase import firebase
 import re
-
+#import bcrypt
 
 firebase=firebase.FirebaseApplication("https://chat-app-login.firebaseio.com/", None)
 import re
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+#regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+#regex='^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
+regex='[^@]+@[^@]+\.[^@]+'
 def check(email):
     if(re.search(regex,email)):  
         return 0           
@@ -13,6 +15,9 @@ def check(email):
 
 def push(u,e,p):
         try:
+            #salt=bcrypt.gensalt()
+            #hashedpass=bcrypt.hashpw(p.encode(),salt)
+            #hashedpass=hashedpass.decode()
             data={
             'Name':u,
             'Email':e,
@@ -30,7 +35,8 @@ def push(u,e,p):
                 else:
                     result=firebase.post("/chat-app-login/User",data)
                     return 1
-        except:
+        except Exception as E:
+            #print(E)
             return -1
 
 
--- libs/firebase_verify.py
+++ libs/firebase_verify.py
@@ -1,16 +1,19 @@
 from firebase import firebase
-
+#import bcrypt
 
 firebase=firebase.FirebaseApplication("https://chat-app-login.firebaseio.com/", None)
 def check(e,p):
         try:
             stored=firebase.get("/chat-app-login/User",'')
             if(not bool(stored)):
-                result=firebase.post("/chat-app-login/User",data)
                 return ('',0)
             for i in stored:
+                #hashed=stored[i]['Password'].encode()
+                #p=p.encode()
+                #hashed=hashed.encode()
                 if(stored[i]['Email']==e and stored[i]['Password']==p):
                     return (stored[i]['Name'],1)
             return ('',0)
-        except:
+        except Exception as E:
+            #print(E)
             return ('',-1)
